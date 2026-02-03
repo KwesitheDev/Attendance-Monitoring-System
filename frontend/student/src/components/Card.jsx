eb30a82104424a2870b9101d3132a905695a7097
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-03 18:51:54 +0000

    refactor: reusable Card component to replace Cards generally used

---
+++ frontend/student/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = ({ children, className = "", ...props }) => {
+  return (
+    <div
+      className={`
+        bg-white p-6 rounded-lg 
+        shadow-md w-full max-w-sm
+        ${className}
+        `}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
