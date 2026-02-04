bb3f8bfdf493e89e0e5a00e747d8a9aead6805f2
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-04 12:01:32 +0000

    style: card border changed to 200

--- frontend/student/src/components/Card.jsx
+++ frontend/student/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ children, className = "", ...props }) => {
     <div
       className={`
         bg-white p-6 rounded-2xl 
-         w-full max-w-sm border border-violet-100
+         w-full max-w-sm border border-violet-200
         ${className}
         `}
       {...props}
