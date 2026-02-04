83278f7b4a2e06dd6d424c004a227fc5abafd1c8
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-04 12:50:38 +0000

    style: Logo added

---
+++ frontend/student/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="text-3xl font-bold text-white bg-violet-500 px-8 py-6 rounded-lg">
+      A
+    </div>
+  );
+};
+
+export default Logo;
