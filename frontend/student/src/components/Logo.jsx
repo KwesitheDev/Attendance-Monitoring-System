968e20961270ba5639fb4ae9cb7742c14212aef9
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-05 09:38:28 +0000

    refactor: className prop added

--- frontend/student/src/components/Logo.jsx
+++ frontend/student/src/components/Logo.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Logo = () => {
+const Logo = ({ className = "" }) => {
   return (
-    <div className="text-3xl font-bold text-white bg-indigo-500 px-6 py-4 rounded-2xl">
+    <div
+      className={`text-3xl font-bold text-white bg-indigo-500 px-6 py-4 rounded-2xl ${className}`}
+    >
       A
     </div>
   );
