864a4d3fe27cf5691071b62dcf3e437644f61d61
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-06 11:03:40 +0000

    style: progress bar added

---
+++ frontend/student/src/components/ProgressBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProgressBar = ({ current, total }) => {
+  const percentage =
+    current && total > 0 ? ((current / total) * 100).toFixed(2) : 0;
+
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-4">
+      <div
+        className="bg-blue-500 h-4 rounded-full"
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
