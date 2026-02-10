a00dabb80b5d4373b51c6415d94c40ed081114cf
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-10 17:48:04 +0000

    feat: added className props to both divs

--- frontend/student/src/components/ProgressBar.jsx
+++ frontend/student/src/components/ProgressBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ProgressBar = ({ current, total }) => {
+const ProgressBar = ({ current, total, className = "", innerClass = "" }) => {
   const percentage =
     current && total > 0 ? ((current / total) * 100).toFixed(2) : 0;
 
   return (
-    <div className="w-full bg-gray-200 rounded-full h-4">
+    <div className={`w-full bg-gray-200 rounded-full ${className} `}>
       <div
-        className="bg-indigo-500 h-4 rounded-full"
+        className={`bg-indigo-500 h-4 rounded-full ${innerClass} `}
         style={{ width: `${percentage}%` }}
       ></div>
     </div>
