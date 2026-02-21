77d68b0617840776a18d76ec2a8ac08fdf892f49
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-21 20:12:53 +0000

    fix: fixed conditional check which could return a boolean instead of a numeric check

--- frontend/student/src/components/ProgressBar.jsx
+++ frontend/student/src/components/ProgressBar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const ProgressBar = ({ current, total, className = "", innerClass = "" }) => {
-  const percentage =
-    current && total > 0 ? ((current / total) * 100).toFixed(2) : 0;
-
+  const percentage = total > 0 ? ((current / total) * 100).toFixed(2) : 0;
   return (
     <div className={`w-full bg-gray-200 rounded-full ${className} `}>
       <div
