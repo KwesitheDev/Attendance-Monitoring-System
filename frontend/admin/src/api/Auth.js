6bff309f28659aedcf4d20c6b408d53f850bb8e6
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-02-26 19:16:23 +0000

    feat: added user to localStorage

--- frontend/admin/src/api/Auth.js
+++ frontend/admin/src/api/Auth.js
@@ -1,21 +1,34 @@
-import api from './apiConfig';
+import api from "./apiConfig";
 
 export const login = async (credentials) => {
-    const response = await api.post('/auth/login', credentials);
-    return response.data;
+  const response = await api.post("/auth/login", credentials);
+  if (response.data.token) {
+    localStorage.setItem("token", response.data.token);
+
+    const safeUser = {
+      id: response.data.user.id,
+      email: response.data.user.email,
+      name: response.data.user.name,
+      role: response.data.user.role,
+      department: response.data.user.department,
+    };
+
+    localStorage.setItem("user", JSON.stringify(safeUser));
+  }
+  return response.data;
 };
 
 export const getUser = async () => {
-    const response = await api.get('/auth/user');
-    return response.data;
+  const response = await api.get("/auth/user");
+  return response.data;
 };
 
 export const register = async (userData) => {
-    const response = await api.post('/auth/register', userData);
-    return response.data;
+  const response = await api.post("/auth/register", userData);
+  return response.data;
 };
 
 export const getDepartments = async () => {
-    const response = await api.get('/auth/departments');
-    return response.data;
+  const response = await api.get("/auth/departments");
+  return response.data;
 };
